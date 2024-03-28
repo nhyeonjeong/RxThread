6e9042eaafd5795b35af07282981eb26d6fd3969
nhyeonjeong <hjnam5962@gmail.com>
2024-03-29 07:40:24 +0900

    [FEAT] 비밀번호 입력 6자 초과 16자 미만일때만 다음버튼 활성화 및 연두색 변경

--- SeSACRxThreads/SignUp/PasswordViewController.swift
+++ SeSACRxThreads/SignUp/PasswordViewController.swift
@@ -7,11 +7,18 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PasswordViewController: UIViewController {
    
     let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+
+    let disposeBag = DisposeBag()
+    lazy var passwordObservable = Observable.just(passwordTextField.text)
+    let nextButtonObservable = BehaviorSubject(value: false) // 이벤트를 주기만?
+    let nextButtonColor = BehaviorSubject(value: UIColor.gray)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +27,39 @@ class PasswordViewController: UIViewController {
         
         configureLayout()
          
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        
+        // textfield.text의 글자수에 따른 nextButton.isEnabled변경
+        passwordTextField.rx.text
+            .bind(with: self) { owner, text in
+                guard let text else { return }
+                if text.count < 16 && text.count > 6 {
+                    owner.nextButtonObservable.onNext(true)
+                    owner.nextButtonColor.onNext(.green)
+                } else {
+                    owner.nextButtonObservable.onNext(false)
+                    owner.nextButtonColor.onNext(.gray)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        // nextButton 컬러 변경
+        nextButtonColor
+            .bind(to: nextButton.rx.backgroundColor)
+            .disposed(by: disposeBag)
+        
+        // nextButton.isEnabled변경
+        nextButtonObservable
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        // 다음 화면으로 넘기기
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                
+                owner.navigationController?.pushViewController(PhoneViewController(), animated: true)
+                
+            }
+            .disposed(by: disposeBag)
     }
     
     @objc func nextButtonClicked() {
