681b0fcc71c62a6571365905f6944365602b4ea2
nhyeonjeong <hjnam5962@gmail.com>
2024-04-04 00:11:41 +0900

    [REFACTOR] 회원가입 - 비밀번호 viewModel로 rxSwift분리

---
+++ SeSACRxThreads/SignUp/PasswordViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  PasswordViewModel.swift
+//  SeSACRxThreads
+//
+//  Created by 남현정 on 2024/04/03.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class PasswordViewModel {
+    
+    // input
+    // textfield가 입력될 때마다
+    let textFieldRelay = PublishRelay<String>()
+    
+    // output
+    let outputNextButtonIsEnabled = BehaviorRelay(value: false) // 이벤트를 주기만?
+    // import UIKit이 안 되어 있어서 UIColor타입으로 할 수 없었다
+    let outputNextButtonColor = BehaviorRelay(value: false)
+    
+    let disposeBag = DisposeBag()
+    
+    init() {
+        bind()
+    }
+    
+    private func bind() {
+        textFieldRelay
+            .asDriver(onErrorJustReturn: "")
+            .drive(with: self) { owner, text in
+                if text.count < 16 && text.count > 6 {
+                    owner.outputNextButtonIsEnabled.accept(true)
+                    owner.outputNextButtonColor.accept(true)
+                } else {
+                    owner.outputNextButtonIsEnabled.accept(false)
+                    owner.outputNextButtonColor.accept(false)
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+}
